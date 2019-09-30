c79e26b2ca38473c1a50eff67b876e203d116d60
Irineu A. Silva <mmirineusilva@gmail.com>
2019-09-30 18:02:17 -0300

    Começando, algumas ideias

--- main.c
+++ main.c
@@ -23,13 +23,58 @@ int e_inteiro()
     return temp;
 }
 
+typedef struct user
+{
+    char login[10];
+    char senha[10];
+}user;
+
+//declaração de funções
+void menu_principal();
+user* entrar();
+
 int main()
 {
+    user *u1;
     setlocale(LC_ALL, "portuguese");
-    printf("\n1.Jogar  \t|[1]Fulando\n2.Scores\t|+10 vitórias\n");
-    // printf("\nModo: (1) Computador\n      (2) Dois jogadores\n");
-
-    //printf("\nScore:\n[1]Fulano +10 vitórias\n[2]Sicrano -5 vitórias\n");
+    u1=entrar();
+    menu_principal(u1);
 
     return 0;
+}
+
+user* entrar()
+{
+    user *temp;
+    FILE *usuarios;
+    fopen("users.txt", "r");
+}
+
+user* criar()
+{
+
+}
+
+void menu_principal()
+{
+    int op;
+    do
+    {
+        printf("\n1.Jogar  \t|[1]Fulando\n2.Scores\t|+10 vitórias\n3.Sair   \t|15-5 derrotas\n");
+        op=e_inteiro();
+        switch(op)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            break;
+        }
+    }
+    while (op!=3);
 }
